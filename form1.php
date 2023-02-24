080841d8514e2fc78869c6c280f74f21be19b28c
DANOUN Hayet <hayetdanoun@gmail.com>
2023-02-24 08:47:36 +0100

    Final exo1

--- form1.php
+++ form1.php
@@ -38,7 +38,7 @@
 
   <?php
     echo  'Merci ' . $_POST['user_prenom'] . ' ' . $_POST['user_name'] . ' de nous avoir contacté à propos de ' . $_POST['user_voyage'] .'<br>' ;
-    echo  'Un de nos conseillers vous contactera soit à l\'adresse ' . $_POST['user_email']  . ' ou par téléphone au '. $_POST['user_phone'] .
+    echo  'Un de nos conseillers vous contactera soit à l'adresse ' . $_POST['user_email']  . ' ou par téléphone au '. $_POST['user_phone'] .
           ' dans les plus brefs délais pour traiter votre demande : <br>' . $_POST['user_message'] ;
 
   ?>
